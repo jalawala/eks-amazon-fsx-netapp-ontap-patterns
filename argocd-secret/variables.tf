ef2be7e4a254e8a7e429ec2bbe1075fe98e148ec
jalawala <alawalajp@gmail.com>
2023-08-12 09:22:36 +0000

    Update

---
+++ argocd-secret/variables.tf
@@ -0,0 +1,18 @@
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "argocd_secret_manager_name_suffix" {
+  type        = string
+  description = "Name of secret manager secret for ArgoCD Admin UI Password"
+  default     = "argocd-admin-secret"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS Cluster"
+  type        = string
+  default     = "eks-amazon-fsx-netapp1"
+}
